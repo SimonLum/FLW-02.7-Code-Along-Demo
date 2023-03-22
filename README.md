08a727b99a3cb54364bd921d127a28d2376bb73d
SimonLum <115673264+SimonLum@users.noreply.github.com>
2023-03-22 23:33:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## FLW 02.7 Code Along
+
+1. Change 'favs' values to your own using dot notation.
+
+2. Add at least 2 additional favorites (e.g. movie, tv show, videogame) to the object `favs` using dot notation.
+
+Be sure to click 'Run' to see your changes.
---
+++ script.js
@@ -0,0 +1,38 @@
+const favs = {
+    artist: "Céline Dion",
+    food: "pho",
+    animal: "wombat",
+    number: 37
+};
+
+
+//start coding here
+let newArtist = "Fujii Kaze";
+favs.artist = newArtist;
+let newFood = "Rice";
+favs.food = newFood;
+let newAnimal = "Cat";
+favs.animal = newAnimal;
+let newNumber = 2
+favs.number = newNumber;
+let movie = "Bullet Train";
+favs.movie = movie
+let anime = "ID:Invaded";
+favs.anime = anime
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DON'T CHANGE CODE BELOW THIS LINE
+for (let key in favs) {
+    document.querySelector(".favs").insertAdjacentHTML('beforeend', `<p>${key}: ${favs[key]}</p>`);
+}
